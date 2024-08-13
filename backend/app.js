2471c90c7ad103c2c69461f7463909a652278062
Amritapa Mondal <amritapamondal@gmail.com>
2024-08-14 01:05:43 +0530

    cors added

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const propertyRoutes = require("./routes/propertyRoutes");
 const userRoutes = require("./routes/userRoutes");
 const cookieParser = require("cookie-parser");
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
